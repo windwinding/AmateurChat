68be739389b50e701f4e43a72f21660f5ad997e6
windwinding <117351233+windwinding@users.noreply.github.com>
2020-09-27 20:06:14 +0000

    Adjust comment

--- core/src/main/java/com/coinomi/core/Preconditions.java
+++ core/src/main/java/com/coinomi/core/Preconditions.java
@@ -133,4 +133,35 @@ public final class Preconditions {
      * @param expression a boolean expression
      * @param errorMessageTemplate a template for the exception message should the check fail. The
      *     message is formed by replacing each {@code %s} placeholder in the template with an
-     *     argument. These are 
+     *     argument. These are matched by position - the first {@code %s} gets {@code
+     *     errorMessageArgs[0]}, etc.  Unmatched arguments will be appended to the formatted message
+     *     in square braces. Unmatched placeholders will be left as-is.
+     * @param errorMessageArgs the arguments to be substituted into the message template. Arguments
+     *     are converted to strings using {@link String#valueOf(Object)}.
+     * @throws IllegalArgumentException if {@code expression} is false
+     * @throws NullPointerException if the check fails and either {@code errorMessageTemplate} or
+     *     {@code errorMessageArgs} is null (don't let this happen)
+     */
+    public static void checkArgument(boolean expression,
+                                     @Nullable String errorMessageTemplate,
+                                     @Nullable Object... errorMessageArgs) {
+        if (!expression) {
+            throw new IllegalArgumentException(format(errorMessageTemplate, errorMessageArgs));
+        }
+    }
+
+    /**
+     * Ensures the truth of an expression involving the state of the calling instance, but not
+     * involving any parameters to the calling method.
+     *
+     * @param expression a boolean expression
+     * @throws IllegalStateException if {@code expression} is false
+     */
+    public static void checkState(boolean expression) {
+        if (!expression) {
+            throw new IllegalStateException();
+        }
+    }
+
+    /**
+     * Ensures the truth of an expression i
