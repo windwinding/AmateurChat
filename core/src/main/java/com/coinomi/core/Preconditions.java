367cad6b365dc07c14905666e586094ba515a94b
windwinding <117351233+windwinding@users.noreply.github.com>
2020-10-20 10:06:28 +0000

    adds .travis.yml

--- core/src/main/java/com/coinomi/core/Preconditions.java
+++ core/src/main/java/com/coinomi/core/Preconditions.java
@@ -226,4 +226,35 @@ public final class Preconditions {
      * @throws NullPointerException if {@code reference} is null
      */
     public static <T> T checkNotNull(T reference, @Nullable Object errorMessage) {
-        if
+        if (reference == null) {
+            throw new NullPointerException(String.valueOf(errorMessage));
+        }
+        return reference;
+    }
+
+    /**
+     * Ensures that an object reference passed as a parameter to the calling method is not null.
+     *
+     * @param reference an object reference
+     * @param errorMessageTemplate a template for the exception message should the check fail. The
+     *     message is formed by replacing each {@code %s} placeholder in the template with an
+     *     argument. These are matched by position - the first {@code %s} gets {@code
+     *     errorMessageArgs[0]}, etc.  Unmatched arguments will be appended to the formatted message
+     *     in square braces. Unmatched placeholders will be left as-is.
+     * @param errorMessageArgs the arguments to be substituted into the message template. Arguments
+     *     are converted to strings using {@link String#valueOf(Object)}.
+     * @return the non-null reference that was validated
+     * @throws NullPointerException if {@code reference} is null
+     */
+    public static <T> T checkNotNull(T reference,
+                                     @Nullable String errorMessageTemplate,
+                                     @Nullable Object... errorMessageArgs) {
+        if (reference == null) {
+            // If either of these parameters is null, the right thing happens anyway
+            throw new NullPointerException(format(errorMessageTemplate, errorMessageArgs));
+        }
+        return reference;
+    }
+
+  /*
+   * All recent hotspots (as of 2009) *re
