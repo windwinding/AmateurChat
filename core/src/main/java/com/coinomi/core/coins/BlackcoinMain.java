3e4b2ec7311c136ea35aeff7316e8c513cab3244
windwinding <117351233+windwinding@users.noreply.github.com>
2020-11-19 06:23:20 +0000

    Bump version to v2.0.0

--- core/src/main/java/com/coinomi/core/coins/BlackcoinMain.java
+++ core/src/main/java/com/coinomi/core/coins/BlackcoinMain.java
@@ -17,3 +17,18 @@ public class BlackcoinMain extends PeerFamily {
 
         name = "Blackcoin";
         symbol = "BLK";
+        uriScheme = "blackcoin";
+        bip44Index = 10;
+        unitExponent = 8;
+        feeValue = value(10000); // 0.0001 BLK
+        minNonDust = value(1);
+        softDustLimit = value(1000000); // 0.01 BLK
+        softDustPolicy = SoftDustPolicy.AT_LEAST_BASE_FEE_IF_SOFT_DUST_TXO_PRESENT;
+        signedMessageHeader = toBytes("BlackCoin Signed Message:\n");
+    }
+
+    private static BlackcoinMain instance = new BlackcoinMain();
+    public static synchronized CoinType get() {
+        return instance;
+    }
+}
---
+++ core/src/main/java/com/coinomi/core/coins/ClamsMain.java
@@ -0,0 +1,13 @@
+package com.coinomi.core.coins;
+
+import com.coinomi.core.coins.families.ClamsFamily;
+
+/**
+ * @author John L. Jegutanis
+ */
+public class ClamsMain extends ClamsFamily {
+    private ClamsMain() {
+        id = "clams.main";
+
+        addressHeader = 137;
+        p2sh
