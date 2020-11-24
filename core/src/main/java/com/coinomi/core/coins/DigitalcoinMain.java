c875a2a6bfeadb9a246218f277ff42d3b56a239b
windwinding <117351233+windwinding@users.noreply.github.com>
2020-11-24 15:19:34 +0000

    Update depends

--- core/src/main/java/com/coinomi/core/coins/DigitalcoinMain.java
+++ core/src/main/java/com/coinomi/core/coins/DigitalcoinMain.java
@@ -11,4 +11,25 @@ public class DigitalcoinMain extends BitFamily {
 
         addressHeader = 30;
         p2shHeader = 5;
-        acceptableAddressC
+        acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
+        spendableCoinbaseDepth = 5;
+        dumpedPrivateKeyHeader = 158;
+
+        name = "Digitalcoin";
+        symbol = "DGC";
+        uriScheme = "digitalcoin";
+        bip44Index = 18;
+        unitExponent = 8;
+        feeValue = value(5000000); // 0.05 DGC
+        feePolicy = FeePolicy.FLAT_FEE;
+        minNonDust = value(10920);
+        softDustLimit = minNonDust;
+        softDustPolicy = SoftDustPolicy.NO_POLICY;
+        signedMessageHeader = toBytes("Digitalcoin Signed Message:\n");
+    }
+
+    private static DigitalcoinMain instance = new DigitalcoinMain();
+    public static synchronized CoinType get() {
+        return instance;
+    }
+}
