6d0fe216c805bdf01e824a008b17136557a49523
windwinding <117351233+windwinding@users.noreply.github.com>
2020-11-24 21:12:46 +0000

    Fix Coverity

--- core/src/main/java/com/coinomi/core/coins/EguldenMain.java
+++ core/src/main/java/com/coinomi/core/coins/EguldenMain.java
@@ -10,3 +10,22 @@ public class EguldenMain extends BitFamily {
         p2shHeader = 5;
         acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
         spendableCoinbaseDepth = 100;
+        dumpedPrivateKeyHeader = 176;
+
+        name = "e-Gulden (beta)";
+        symbol = "EFL";
+        uriScheme = "egulden";
+        bip44Index = 2;
+        unitExponent = 8;
+        feeValue = value(100000);
+        minNonDust = value(1000); // 0.00001 EFL mininput
+        softDustLimit = value(100000); // 0.001 EFL
+        softDustPolicy = SoftDustPolicy.BASE_FEE_FOR_EACH_SOFT_DUST_TXO;
+        signedMessageHeader = toBytes("e-Gulden Signed Message:\n");
+    }
+
+    private static EguldenMain instance = new EguldenMain();
+    public static synchronized EguldenMain get() {
+        return instance;
+    }
+}
