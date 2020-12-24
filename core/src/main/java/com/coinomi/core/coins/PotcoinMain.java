88dd1e41f5eb1e46f71b8b7f5fe5553f5be2d3f8
windwinding <117351233+windwinding@users.noreply.github.com>
2020-12-24 09:38:34 +0000

    Add contributors file

---
+++ core/src/main/java/com/coinomi/core/coins/PotcoinMain.java
@@ -0,0 +1,28 @@
+package com.coinomi.core.coins;
+
+import com.coinomi.core.coins.families.ReddFamily;
+
+/**
+ * @author L33tConsultant
+ */
+public class PotcoinMain extends ReddFamily {
+
+    private PotcoinMain() {
+        id = "potcoin.main";
+
+        addressHeader = 55;
+        p2shHeader = 5;
+        acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
+        spendableCoinbaseDepth = 100;
+        transactionVersion = 4;
+        dumpedPrivateKeyHeader = 189;
+
+        name = "Potcoin (beta)";
+        symbol = "POT";
+        uriScheme = "potcoin";
+        bip44Index = 81;
+        unitExponent = 8;
+        feeValue = value(100000);
+        minNonDust = value(1000);
+        softDustLimit = value(100000);
+        softDustPolicy = SoftDustPolicy.BASE_FEE_FOR_EACH
