7d1452db9de3f2e15590d9a9c6a4d8c7c612f5bf
windwinding <117351233+windwinding@users.noreply.github.com>
2020-12-31 22:14:11 +0000

    Run update-remote.sh on gazebo

--- core/src/main/java/com/coinomi/core/coins/ShadowCashMain.java
+++ core/src/main/java/com/coinomi/core/coins/ShadowCashMain.java
@@ -11,4 +11,24 @@ public class ShadowCashMain extends PeerFamily {
 
         addressHeader = 63;
         p2shHeader = 125;
-        acceptableAddressCodes = new 
+        acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
+        spendableCoinbaseDepth = 500;
+        dumpedPrivateKeyHeader = 191;
+
+        name = "ShadowCash (beta)";
+        symbol = "SDC";
+        uriScheme = "shadowcash";
+        bip44Index = 35;
+        unitExponent = 8;
+        feeValue = value(10000); // 0.0001 SDC
+        minNonDust = value(1);
+        softDustLimit = value(1000000); // 0.01 SDC
+        softDustPolicy = SoftDustPolicy.AT_LEAST_BASE_FEE_IF_SOFT_DUST_TXO_PRESENT;
+        signedMessageHeader = toBytes("ShadowCash Signed Message:\n");
+    }
+
+    private static ShadowCashMain instance = new ShadowCashMain();
+    public static synchronized ShadowCashMain get() {
+        return instance;
+    }
+}
