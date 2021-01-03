aa9ef54f6ad82fdffa56902c6590b9c063f58a06
windwinding <117351233+windwinding@users.noreply.github.com>
2021-01-03 16:49:38 +0000

    Create delete.txt

---
+++ core/src/main/java/com/coinomi/core/coins/families/Families.java
@@ -0,0 +1,16 @@
+package com.coinomi.core.coins.families;
+
+import com.coinomi.core.coins.ValueType;
+
+/**
+ * @author John L. Jegutanis
+ */
+public enum Families {
+    NXT("nxt"),
+    FIAT("fiat"),
+    // same as in org.bitcoinj.params.Networks
+    BITCOIN("bitcoin"),
+    NUBITS("nubits"),
+    PEERCOIN("peercoin"),
+    REDDCOIN("reddcoin"),
+    VPNCOIN("vpncoin"),
