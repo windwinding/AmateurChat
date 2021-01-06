7aa5f62c581107cb925455f68eac071ae16911a4
windwinding <117351233+windwinding@users.noreply.github.com>
2021-01-06 21:47:03 +0000

    cpufreq: Optimize cpufreq_frequency_table_verify()

--- core/src/main/java/com/coinomi/core/coins/nxt/Constants.java
+++ core/src/main/java/com/coinomi/core/coins/nxt/Constants.java
@@ -7,4 +7,29 @@ package com.coinomi.core.coins.nxt;
  * the top-level directory of this distribution for the individual copyright  *
  * holder information and the developer policies on copyright and licensing.  *
  *                                                                            *
- *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * Nxt software, including this file, may be copied, modified, propagated,    *
+ * or distributed except according to the terms contained in the LICENSE.txt  *
+ * file.                                                                      *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public final class Constants {
+
+//    public static final boolean isTestnet = Nxt.getBooleanProperty("nxt.isTestnet");
+//    public static final boolean isOffline = Nxt.getBooleanProperty("nxt.isOffline");
+
+    public static final int MAX_NUMBER_OF_TRANSACTIONS = 255;
+    public static final int MAX_PAYLOAD_LENGTH = MAX_NUMBER_OF_TRANSACTIONS * 176;
+    public static final long MAX_BALANCE_NXT = 1000000000;
+    public static final long ONE_NXT = 100000000;
+    public static final long MAX_BALANCE_NQT = MAX_BALANCE_NXT * ONE_NXT;
+    //public static final long INITIAL_BASE_TARGET = 153722867;
+    //public static final long MAX_BASE_TARGET = MAX_BALANCE_NXT * INITIAL_BASE_TARGET;
+//    public static final int MAX_ROLLBACK = Math.max(Nxt.getIntProperty("nxt.maxRollback"), 720);
+//    public static final int GUARANTEED_BALANCE
