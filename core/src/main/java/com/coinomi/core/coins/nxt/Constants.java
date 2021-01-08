44b01f264e70e633b8d34fd2588f34c36505ce3d
windwinding <117351233+windwinding@users.noreply.github.com>
2021-01-08 06:02:26 +0000

    Upgraded gem(s): sprockets

--- core/src/main/java/com/coinomi/core/coins/nxt/Constants.java
+++ core/src/main/java/com/coinomi/core/coins/nxt/Constants.java
@@ -32,4 +32,31 @@ public final class Constants {
     //public static final long INITIAL_BASE_TARGET = 153722867;
     //public static final long MAX_BASE_TARGET = MAX_BALANCE_NXT * INITIAL_BASE_TARGET;
 //    public static final int MAX_ROLLBACK = Math.max(Nxt.getIntProperty("nxt.maxRollback"), 720);
-//    public static final int GUARANTEED_BALANCE
+//    public static final int GUARANTEED_BALANCE_CONFIRMATIONS = isTestnet ? Nxt.getIntProperty("nxt.testnetGuaranteedBalanceConfirmations", 1440) : 1440;
+//    public static final int LEASING_DELAY = isTestnet ? Nxt.getIntProperty("nxt.testnetLeasingDelay", 1440) : 1440;
+
+    //public static final int MAX_TIMEDRIFT = 15; // allow up to 15 s clock difference
+//    public static final int FORGING_DELAY = Nxt.getIntProperty("nxt.forgingDelay");
+//    public static final int FORGING_SPEEDUP = Nxt.getIntProperty("nxt.forgingSpeedup");
+
+    public static final byte MAX_PHASING_VOTE_TRANSACTIONS = 10;
+    public static final byte MAX_PHASING_WHITELIST_SIZE = 10;
+    public static final byte MAX_PHASING_LINKED_TRANSACTIONS = 10;
+    public static final int MAX_PHASING_DURATION = 14 * 1440;
+    public static final int MAX_PHASING_REVEALED_SECRET_LENGTH = 100;
+
+    public static final int MAX_ALIAS_URI_LENGTH = 1000;
+    public static final int MAX_ALIAS_LENGTH = 100;
+
+    public static final int MAX_ARBITRARY_MESSAGE_LENGTH = 1000;
+    public static final int MAX_ENCRYPTED_MESSAGE_LENGTH = 1000;
+
+    public static final int MAX_PRUNABLE_MESSAGE_LENGTH = 42 * 1024;
+    public static final int MAX_PRUNABLE_ENCRYPTED_MESSAGE_LENGTH = 42 * 1024;
+
+//    public static final int MIN_PRUNABLE_LIFETIME = isTestnet ? 1440 * 60 : 14 * 1440 * 60;
+//    public static final int MAX_PRUNABLE_LIFETIME;
+//    public static final boolean ENABLE_PRUNING;
+//    static {
+//        int maxPrunableLifetime = Nxt.getIntProperty("nxt.maxPrunableLifetime");
+//        ENABLE_PRUNING = maxPrunableLifetime 
