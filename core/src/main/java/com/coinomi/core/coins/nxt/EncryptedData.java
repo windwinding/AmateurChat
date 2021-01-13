b10af9064935efa183d29f5eb9f84247654b48da
windwinding <117351233+windwinding@users.noreply.github.com>
2021-01-13 18:33:41 +0000

    Removed old comment

--- core/src/main/java/com/coinomi/core/coins/nxt/EncryptedData.java
+++ core/src/main/java/com/coinomi/core/coins/nxt/EncryptedData.java
@@ -10,4 +10,42 @@ package com.coinomi.core.coins.nxt;
  * Unless otherwise agreed in a custom licensing agreement, no part of the    *
  * Nxt software, including this file, may be copied, modified, propagated,    *
  * or distributed except according to the terms contained in the LICENSE.txt  *
- * file.                               
+ * file.                                                                      *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.security.SecureRandom;
+
+public final class EncryptedData {
+
+    private static final ThreadLocal<SecureRandom> secureRandom = new ThreadLocal<SecureRandom>() {
+        @Override
+        protected SecureRandom initialValue() {
+            return new SecureRandom();
+        }
+    };
+
+    public static final EncryptedData EMPTY_DATA = new EncryptedData(new byte[0], new byte[0]);
+
+    public static EncryptedData encrypt(byte[] plaintext, byte[] myPrivateKey, byte[] theirPublicKey) {
+        if (plaintext.length == 0) {
+            return EMPTY_DATA;
+        }
+        byte[] nonce = new byte[32];
+        secureRandom.get().nextBytes(nonce);
+        byte[] data = Crypto.aesEncrypt(plaintext, myPrivateKey, theirPublicKey, nonce);
+        return new EncryptedData(data, nonce);
+    }
+
+    public static EncryptedData readEncryptedData(ByteBuffer buffer, int length, int maxLength)
+            throws NxtException.NotValidException {
+        if (length == 0) {
+            return EMPTY_DATA;
+        }
+        if (length > maxLength) {
+            throw new NxtException.NotValidException("Max encrypted data length exceeded: " + length);
