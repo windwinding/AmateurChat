4ccf2b5c0295c45c879328520ab5e3bd9d7e1188
windwinding <117351233+windwinding@users.noreply.github.com>
2021-01-20 22:47:43 +0000

    Update HANDSON.md

--- core/src/main/java/com/coinomi/core/coins/nxt/Transaction.java
+++ core/src/main/java/com/coinomi/core/coins/nxt/Transaction.java
@@ -12,4 +12,84 @@ public interface Transaction extends Comparable<Transaction> {
 
         Builder referencedTransactionFullHash(String referencedTransactionFullHash);
 
-        Builder message(Appendix.M
+        Builder message(Appendix.Message message);
+
+        Builder encryptedMessage(Appendix.EncryptedMessage encryptedMessage);
+
+        Builder encryptToSelfMessage(Appendix.EncryptToSelfMessage encryptToSelfMessage);
+
+        Builder publicKeyAnnouncement(Appendix.PublicKeyAnnouncement publicKeyAnnouncement);
+
+        Transaction build() throws NxtException.NotValidException;
+
+    }
+
+    void setHeight(int height);
+
+    void setConfirmations(int confirmations);
+
+    long getId();
+
+    String getStringId();
+
+    long getSenderId();
+
+    byte[] getSenderPublicKey();
+
+    long getRecipientId();
+
+    int getHeight();
+
+    int getTimestamp();
+
+    int getConfirmations();
+
+    int getBlockTimestamp();
+
+    short getDeadline();
+
+    int getExpiration();
+
+    long getAmountNQT();
+
+    long getFeeNQT();
+
+    String getReferencedTransactionFullHash();
+
+    byte[] getSignature();
+
+    String getFullHash();
+
+    TransactionType getType();
+
+    Attachment getAttachment();
+
+    void sign(String secretPhrase);
+
+    void sign(byte[] privateKey);
+
+    byte[] getBytes();
+
+    byte[] getUnsignedBytes();
+
+    //JSONObject getJSONObject();
+
+    byte getVersion();
+
+    Appendix.Message getMessage();
+
+    Appendix.EncryptedMessage getEncryptedMessage();
+
+    Appendix.EncryptToSelfMessage getEncryptToSelfMessage();
+
+    List<? extends Appendix> getAppendages();
+
+    /*
+    Collection<TransactionType> getPhasingTransactionTypes();
+
+    Collection<TransactionType> getPhasedTransactionTypes();
+    */
+
+    int getECBlockHeight();
+
+    long getECBlo
