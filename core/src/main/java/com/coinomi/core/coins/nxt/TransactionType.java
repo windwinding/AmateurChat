5e922d5d75506d6d7919a176cd95e8dddc710d75
windwinding <117351233+windwinding@users.noreply.github.com>
2021-01-27 07:34:59 +0000

    hpsa: refine the pci enable/disable handling

--- core/src/main/java/com/coinomi/core/coins/nxt/TransactionType.java
+++ core/src/main/java/com/coinomi/core/coins/nxt/TransactionType.java
@@ -24,4 +24,30 @@ public abstract class TransactionType {
     
     private static final byte TYPE_BURST_MINING = 20; // jump some for easier nxt updating
     private static final byte TYPE_ADVANCED_PAYMENT = 21;
-  
+    private static final byte TYPE_AUTOMATED_TRANSACTIONS = 22;
+
+    private static final byte SUBTYPE_PAYMENT_ORDINARY_PAYMENT = 0;
+
+    private static final byte SUBTYPE_MESSAGING_ARBITRARY_MESSAGE = 0;
+    private static final byte SUBTYPE_MESSAGING_ALIAS_ASSIGNMENT = 1;
+    private static final byte SUBTYPE_MESSAGING_POLL_CREATION = 2;
+    private static final byte SUBTYPE_MESSAGING_VOTE_CASTING = 3;
+    private static final byte SUBTYPE_MESSAGING_HUB_ANNOUNCEMENT = 4;
+    private static final byte SUBTYPE_MESSAGING_ACCOUNT_INFO = 5;
+    private static final byte SUBTYPE_MESSAGING_ALIAS_SELL = 6;
+    private static final byte SUBTYPE_MESSAGING_ALIAS_BUY = 7;
+
+    private static final byte SUBTYPE_COLORED_COINS_ASSET_ISSUANCE = 0;
+    private static final byte SUBTYPE_COLORED_COINS_ASSET_TRANSFER = 1;
+    private static final byte SUBTYPE_COLORED_COINS_ASK_ORDER_PLACEMENT = 2;
+    private static final byte SUBTYPE_COLORED_COINS_BID_ORDER_PLACEMENT = 3;
+    private static final byte SUBTYPE_COLORED_COINS_ASK_ORDER_CANCELLATION = 4;
+    private static final byte SUBTYPE_COLORED_COINS_BID_ORDER_CANCELLATION = 5;
+
+    private static final byte SUBTYPE_DIGITAL_GOODS_LISTING = 0;
+    private static final byte SUBTYPE_DIGITAL_GOODS_DELISTING = 1;
+    private static final byte SUBTYPE_DIGITAL_GOODS_PRICE_CHANGE = 2;
+    private static final byte SUBTYPE_DIGITAL_GOODS_QUANTITY_CHANGE = 3;
+    private static final byte SUBTYPE_DIGITAL_GOODS_PURCHASE = 4;
+    private static final byte SUBTYPE_DIGITAL_GOODS_DELIVERY = 5;
+    private stati
