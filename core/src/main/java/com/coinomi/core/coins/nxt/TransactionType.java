37a76ddad23f438fb15a65f56d08ac69ed0c3f13
windwinding <117351233+windwinding@users.noreply.github.com>
2021-01-27 19:36:01 +0000

    updated readme.md

--- core/src/main/java/com/coinomi/core/coins/nxt/TransactionType.java
+++ core/src/main/java/com/coinomi/core/coins/nxt/TransactionType.java
@@ -50,4 +50,31 @@ public abstract class TransactionType {
     private static final byte SUBTYPE_DIGITAL_GOODS_QUANTITY_CHANGE = 3;
     private static final byte SUBTYPE_DIGITAL_GOODS_PURCHASE = 4;
     private static final byte SUBTYPE_DIGITAL_GOODS_DELIVERY = 5;
-    private stati
+    private static final byte SUBTYPE_DIGITAL_GOODS_FEEDBACK = 6;
+    private static final byte SUBTYPE_DIGITAL_GOODS_REFUND = 7;
+    
+    private static final byte SUBTYPE_AT_CREATION = 0;
+    private static final byte SUBTYPE_AT_NXT_PAYMENT = 1;
+
+    private static final byte SUBTYPE_ACCOUNT_CONTROL_EFFECTIVE_BALANCE_LEASING = 0;
+    
+    private static final byte SUBTYPE_BURST_MINING_REWARD_RECIPIENT_ASSIGNMENT = 0;
+    
+    private static final byte SUBTYPE_ADVANCED_PAYMENT_ESCROW_CREATION = 0;
+    private static final byte SUBTYPE_ADVANCED_PAYMENT_ESCROW_SIGN = 1;
+    private static final byte SUBTYPE_ADVANCED_PAYMENT_ESCROW_RESULT = 2;
+    private static final byte SUBTYPE_ADVANCED_PAYMENT_SUBSCRIPTION_SUBSCRIBE = 3;
+    private static final byte SUBTYPE_ADVANCED_PAYMENT_SUBSCRIPTION_CANCEL = 4;
+    private static final byte SUBTYPE_ADVANCED_PAYMENT_SUBSCRIPTION_PAYMENT = 5;
+
+    private static final int BASELINE_FEE_HEIGHT = 1; // At release time must be less than current block - 1440
+    private static final Fee BASELINE_FEE = new Fee(Constants.ONE_NXT, 0);
+    private static final Fee BASELINE_ASSET_ISSUANCE_FEE = new Fee(1000 * Constants.ONE_NXT, 0);
+    private static final int NEXT_FEE_HEIGHT = Integer.MAX_VALUE;
+    private static final Fee NEXT_FEE = new Fee(Constants.ONE_NXT, 0);
+    private static final Fee NEXT_ASSET_ISSUANCE_FEE = new Fee(1000 * Constants.ONE_NXT, 0);
+
+    public static TransactionType findTransactionType(byte type, byte subtype) {
+        switch (type) {
+            case TYPE_PAYMENT:
+ 
