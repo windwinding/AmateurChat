c76022581265d73dfd34302083197c2e1ecf04d5
windwinding <117351233+windwinding@users.noreply.github.com>
2021-04-18 06:07:46 +0000

    [cleanup] Apply codestyle

---
+++ core/src/main/java/com/coinomi/core/exchange/shapeshift/data/ShapeShiftEmail.java
@@ -0,0 +1,10 @@
+package com.coinomi.core.exchange.shapeshift.data;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * @author John L. Jegutanis
+ */
+public class ShapeShiftEmail  extends ShapeShiftBase {
+    public final Status s
