9d5a512bd03c9340a8331954bb1fda539d1de092
windwinding <117351233+windwinding@users.noreply.github.com>
2021-04-18 19:40:45 +0000

    Update sbt to 1.3.11

--- core/src/main/java/com/coinomi/core/exchange/shapeshift/data/ShapeShiftEmail.java
+++ core/src/main/java/com/coinomi/core/exchange/shapeshift/data/ShapeShiftEmail.java
@@ -7,4 +7,33 @@ import org.json.JSONObject;
  * @author John L. Jegutanis
  */
 public class ShapeShiftEmail  extends ShapeShiftBase {
-    public final Status s
+    public final Status status;
+    public final String message;
+
+    public static enum Status {
+        SUCCESS, UNKNOWN
+    }
+
+    public ShapeShiftEmail(JSONObject data) throws ShapeShiftException {
+        super(data);
+        if (!isError) {
+            try {
+                JSONObject innerData = data.getJSONObject("email");
+                message = innerData.getString("message");
+                String statusStr = innerData.getString("status");
+                switch (statusStr) {
+                    case "success":
+                        status = Status.SUCCESS;
+                        break;
+                    default:
+                        status = Status.UNKNOWN;
+                }
+            } catch (JSONException e) {
+                throw new ShapeShiftException("Could not parse object", e);
+            }
+        } else {
+            status = null;
+            message = null;
+        }
+    }
+}
