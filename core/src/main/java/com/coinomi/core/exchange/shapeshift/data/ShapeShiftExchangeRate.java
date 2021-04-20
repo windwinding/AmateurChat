9217bdaeb8d14c32cb7f3de3648e3049f08c2fee
windwinding <117351233+windwinding@users.noreply.github.com>
2021-04-20 22:43:59 +0000

    apt-get update

--- core/src/main/java/com/coinomi/core/exchange/shapeshift/data/ShapeShiftExchangeRate.java
+++ core/src/main/java/com/coinomi/core/exchange/shapeshift/data/ShapeShiftExchangeRate.java
@@ -13,4 +13,53 @@ package com.coinomi.core.exchange.shapeshift.data;
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions an
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import com.coinomi.core.coins.CoinType;
+import com.coinomi.core.coins.Value;
+import com.coinomi.core.coins.ValueType;
+import com.coinomi.core.util.ExchangeRateBase;
+
+import org.bitcoinj.core.Coin;
+
+import javax.annotation.Nullable;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+
+/**
+ * An exchange rate is expressed as a ratio of a pair of {@link com.coinomi.core.coins.Value} amounts.
+ */
+public class ShapeShiftExchangeRate extends ExchangeRateBase {
+    public final Value minerFee;
+
+    public ShapeShiftExchangeRate(Value deposit, Value withdraw, Value minerFee) {
+        super(deposit, withdraw);
+        if (minerFee != null) checkArgument(withdraw.type.equals(minerFee.type));
+        this.minerFee = minerFee;
+    }
+
+    public ShapeShiftExchangeRate(ValueType depositType, ValueType withdrawType, String rateString,
+                                  String minerFeeString) {
+        super(depositType, withdrawType, rateString);
+
+        if (minerFeeString != null) {
+            minerFee = withdrawType.value(minerFeeString);
+        } else {
+            minerFee = null;
+        }
+    }
+
+    @Override
+    public Value convert(CoinType type, Coin coin) {
+        return convert(Value.valueOf(type, coin));
+    }
+
+    @Override
+    public Value convert(Value convertingValue) {
+        Value converted = convertValue(convertingValue);
+        if (!converted.isZero() && minerFee != null) {
+    
