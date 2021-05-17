66a482cc9c092677c83538ec9c0f25425b0d3b6d
windwinding <117351233+windwinding@users.noreply.github.com>
2021-05-17 21:42:55 +0000

    msm: mdss: fix sync fence deadlock when rotator is used

---
+++ core/src/main/java/com/coinomi/core/util/HardwareSoftwareCompliance.java
@@ -0,0 +1,11 @@
+package com.coinomi.core.util;
+
+import org.bitcoinj.core.ECKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author John L. Jegutanis
+ */
+public class HardwareSoftwareCompliance {
+    private static final Logger log = LoggerFactory.getLogger(HardwareSoftwareCompliance.clas
