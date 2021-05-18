cc9b3b5225dc9f1d0a4683bfc55cf47cee3f83a9
windwinding <117351233+windwinding@users.noreply.github.com>
2021-05-18 20:23:26 +0000

    remove log

---
+++ core/src/main/java/com/coinomi/core/wallet/AbstractTransaction.java
@@ -0,0 +1,11 @@
+package com.coinomi.core.wallet;
+
+import com.coinomi.core.coins.CoinType;
+import com.coinomi.core.coins.Value;
+import com.coinomi.core.messages.TxMessage;
+
+import org.bitcoinj.core.Sha256Hash;
+import org.bitcoinj.core.TransactionConfidence.ConfidenceType;
+import org.bitcoinj.core.TransactionConfidence.Source;
+
+import java.io.Seria
