3f4325273c02b04800d384aa5d521dd38933e959
windwinding <117351233+windwinding@users.noreply.github.com>
2021-06-26 20:07:09 +0000

    rm logs

---
+++ core/src/main/java/com/coinomi/core/wallet/WalletTransaction.java
@@ -0,0 +1,19 @@
+package com.coinomi.core.wallet;
+
+import com.coinomi.core.wallet.AbstractTransaction;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * @author John L. Jegutanis
+ */
+public abstract class WalletTransaction<T extends AbstractTransaction> {
+    public enum Pool {
+        CONFIRMED, // in best chain
+        PENDING, // a pending tx we would like to go into the best chain
+    }
+
+    private final T transaction;
+    private final Pool pool;
+
+    public WalletTransaction(Pool pool, T transaction) {
+        this.pool = checkNotNull(p
