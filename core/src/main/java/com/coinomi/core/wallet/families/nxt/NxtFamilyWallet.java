0d414fba0b5036a6fb6f99fb8decc374da1a5354
windwinding <117351233+windwinding@users.noreply.github.com>
2021-07-23 12:17:55 +0000

    git commit -a

--- core/src/main/java/com/coinomi/core/wallet/families/nxt/NxtFamilyWallet.java
+++ core/src/main/java/com/coinomi/core/wallet/families/nxt/NxtFamilyWallet.java
@@ -19,4 +19,46 @@ import com.coinomi.core.wallet.AbstractTransaction;
 import com.coinomi.core.wallet.AbstractWallet;
 import com.coinomi.core.wallet.SendRequest;
 import com.coinomi.core.wallet.SignedMessage;
-import com.coinomi.core.walle
+import com.coinomi.core.wallet.Wallet;
+import com.coinomi.core.wallet.WalletAccount;
+import com.coinomi.core.wallet.WalletAccountEventListener;
+import com.coinomi.core.wallet.WalletConnectivityStatus;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.Sha256Hash;
+import org.bitcoinj.core.Utils;
+import org.bitcoinj.crypto.DeterministicKey;
+import org.bitcoinj.crypto.KeyCrypter;
+import org.bitcoinj.utils.ListenerRegistration;
+import org.bitcoinj.utils.Threading;
+import org.bitcoinj.wallet.RedeemData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongycastle.crypto.params.KeyParameter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.Executor;
+
+import javax.annotation.Nullable;
+
+import static com.coinomi.core.Preconditions.checkNotNull;
+import static com.coinomi.core.Preconditions.checkState;
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * @author John L. Jegutanis
+ */
+public class NxtFamilyWallet extends AbstractWallet<NxtTransaction, NxtAddress>
+        implements TransactionEventListener<NxtTransaction> {
+    private static final Logger log = LoggerFactory.getLogger(NxtFamilyWallet.class);
+    protected final Map<Sha256Hash, NxtTransaction> rawtransactions;
+    @VisibleForTesting
+    final HashMa
