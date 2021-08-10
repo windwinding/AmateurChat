b22ddebf00b0dd4c8f1ebadd6defdcfb61418079
windwinding <117351233+windwinding@users.noreply.github.com>
2021-08-10 15:14:52 +0000

    idk

--- core/src/main/java/com/coinomi/core/wallet/families/nxt/NxtSendRequest.java
+++ core/src/main/java/com/coinomi/core/wallet/families/nxt/NxtSendRequest.java
@@ -13,4 +13,37 @@ import com.coinomi.core.coins.nxt.TransactionImpl;
 import com.coinomi.core.util.TypeUtils;
 import com.coinomi.core.wallet.SendRequest;
 
-impo
+import static com.coinomi.core.Preconditions.checkState;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * @author John L. Jegutanis
+ */
+public class NxtSendRequest extends SendRequest<NxtTransaction> {
+    public TransactionImpl.BuilderImpl nxtTxBuilder;
+
+    protected NxtSendRequest(CoinType type) {
+        super(type);
+    }
+
+    public static NxtSendRequest to(NxtFamilyWallet from, NxtAddress destination, Value amount) {
+        checkNotNull(destination.getType(), "Address is for an unknown network");
+        checkState(from.getCoinType() == destination.getType(), "Incompatible destination address coin type");
+        checkState(TypeUtils.is(destination.getType(), amount.type), "Incompatible sending amount type");
+        checkTypeCompatibility(destination.getType());
+
+        NxtSendRequest req = new NxtSendRequest(destination.getType());
+
+        byte version = (byte) 1;
+        int timestamp;
+        if (req.type instanceof NxtMain) {
+            timestamp = Convert.toNxtEpochTime(System.currentTimeMillis());
+        } else if (req.type instanceof BurstMain) {
+            timestamp = Convert.toBurstEpochTime(System.currentTimeMillis());
+        } else {
+            throw new RuntimeException("Unexpected NXT family type: " + req.type.toString());
+        }
+
+        TransactionImpl.BuilderImpl builder = new TransactionImpl.BuilderImpl(version,
+                from.getPublicKey(), amount.value, req.fee.value, timestamp,
+                NxtFamily.DEFAULT_DEADLINE, 
