01a2eeb68590ef400826f2c2bd5f974f4e12d60a
windwinding <117351233+windwinding@users.noreply.github.com>
2021-09-05 19:50:04 +0000

    fixbugs

--- core/src/test/java/com/coinomi/core/exchange/shapeshift/ExchangeRateTest.java
+++ core/src/test/java/com/coinomi/core/exchange/shapeshift/ExchangeRateTest.java
@@ -37,4 +37,31 @@ public class ExchangeRateTest {
         assertEquals(BTC.value("1"), rate.value1);
         assertEquals(DOGE.value("1911057.69230769"), rate.value2);
         assertEquals(DOGE.value("1"), rate.minerFee);
-        assertEquals(BTC.value("0.00052379"), 
+        assertEquals(BTC.value("0.00052379"), rate.convert(DOGE.value("1000")));
+
+        rate = new ShapeShiftExchangeRate(BTC.oneCoin(),
+                DOGE.value("1878207.54716981"), DOGE.value("1"));
+        assertEquals(BTC.value("1"), rate.value1);
+        assertEquals(DOGE.value("1878207.54716981"), rate.value2);
+        assertEquals(DOGE.value("1"), rate.minerFee);
+        assertEquals(BTC.value("0.00532476"), rate.convert(DOGE.value("10000")));
+    }
+
+    @Test
+    public void zeroValues() {
+        ShapeShiftExchangeRate rate = new ShapeShiftExchangeRate(BTC, NBT, "100", "0.01");
+        assertEquals(BTC.value("0"), rate.convert(NBT.value("0")));
+        assertEquals(NBT.value("0"), rate.convert(BTC.value("0")));
+    }
+
+    @Test
+    public void smallValues() {
+        ShapeShiftExchangeRate rate = new ShapeShiftExchangeRate(DOGE, BTC, "5.1e-7", "0.0001");
+        assertEquals(BTC.value("0"), rate.convert(DOGE.value("1")));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void zeroExchangeRate() throws Exception {
+        new ShapeShiftExchangeRate(BTC, NBT, "0", "0.01");
+    }
+}
