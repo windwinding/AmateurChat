64dd97b71776143d4a8f72e798ab5ae6e75223cb
windwinding <117351233+windwinding@users.noreply.github.com>
2021-12-28 18:48:44 +0000

    model upgrade for lex

---
+++ font-icons/demo-files/demo.js
@@ -0,0 +1,15 @@
+if (!('boxShadow' in document.body.style)) {
+	document.body.setAttribute('class', 'noBoxShadow');
+}
+
+document.body.addEventListener("click", function(e) {
+	var target = e.target;
+	if (target.tagName === "INPUT" &&
+		target.getAttribute('class').indexOf('liga') === -1) {
+		target.select();
+	}
+});
+
+(function() {
+	var fontSize = document.getElementById('fontSize'),
+		testDrive = document.getElementBy
