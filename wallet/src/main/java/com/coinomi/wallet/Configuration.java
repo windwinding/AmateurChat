330d02ab625aab5779c5a14bdd7b4d4513ed814b
windwinding <117351233+windwinding@users.noreply.github.com>
2022-03-06 06:19:14 +0000

    fix flow errors

--- wallet/src/main/java/com/coinomi/wallet/Configuration.java
+++ wallet/src/main/java/com/coinomi/wallet/Configuration.java
@@ -29,4 +29,33 @@ public class Configuration {
     private final SharedPreferences prefs;
 
     private static final String PREFS_KEY_LAST_VERSION = "last_version";
-    private static final String PREFS_KEY_LAST_USED = "last_
+    private static final String PREFS_KEY_LAST_USED = "last_used";
+    @Deprecated
+    private static final String PREFS_KEY_LAST_POCKET = "last_pocket";
+    private static final String PREFS_KEY_LAST_ACCOUNT = "last_account";
+
+
+    /* Preference keys. Check also res/xml/preferences.xml */
+    public static final String PREFS_KEY_BTC_PRECISION = "btc_precision";
+    public static final String PREFS_KEY_CONNECTIVITY_NOTIFICATION = "connectivity_notification";
+    public static final String PREFS_KEY_EXCHANGE_CURRENCY = "exchange_currency";
+    public static final String PREFS_KEY_FEES = "fees";
+    public static final String PREFS_KEY_DISCLAIMER = "disclaimer";
+    public static final String PREFS_KEY_SELECTED_ADDRESS = "selected_address";
+
+    private static final String PREFS_KEY_LABS_QR_PAYMENT_REQUEST = "labs_qr_payment_request";
+
+    private static final String PREFS_KEY_CACHED_EXCHANGE_LOCAL_CURRENCY = "cached_exchange_local_currency";
+    private static final String PREFS_KEY_CACHED_EXCHANGE_RATES_JSON = "cached_exchange_rates_json";
+
+    private static final String PREFS_KEY_LAST_EXCHANGE_DIRECTION = "last_exchange_direction";
+    private static final String PREFS_KEY_CHANGE_LOG_VERSION = "change_log_version";
+    public static final String PREFS_KEY_REMIND_BACKUP = "remind_backup";
+
+    public static final String PREFS_KEY_MANUAL_RECEIVING_ADDRESSES = "manual_receiving_addresses";
+
+    public static final String PREFS_KEY_DEVICE_COMPATIBLE = "device_compatible";
+
+    public static final String PREFS_KEY_TERMS_ACCEPTED = "terms_accepted";
+
+    
