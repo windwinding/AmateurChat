c14d923edad41329cb93d98a0f619b1e4e2779e1
windwinding <117351233+windwinding@users.noreply.github.com>
2022-03-14 15:32:53 +0000

    Update help

--- wallet/src/main/java/com/coinomi/wallet/ui/AddCoinsActivity.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/AddCoinsActivity.java
@@ -30,4 +30,43 @@ public class AddCoinsActivity extends BaseWalletActivity
 
     @CheckForNull private Wallet wallet;
     private AddCoinTask addCoinTask;
-   
+    private CoinType selectedCoin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_wrapper);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new SelectCoinsFragment())
+                    .commit();
+        }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+
+        wallet = getWalletApplication().getWallet();
+    }
+
+    @Override
+    public void onCoinSelection(Bundle args) {
+        ArrayList<String> ids = args.getStringArrayList(Constants.ARG_MULTIPLE_COIN_IDS);
+
+        // For new we add only one coin at a time
+        selectedCoin = CoinID.typeFromId(ids.get(0));
+
+        if (wallet.isAccountExists(selectedCoin)) {
+            new AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.coin_already_added_title, selectedCoin.getName()))
+                    .setMessage(R.string.coin_already_added)
+                    .setPositiveButton(R.string.button_ok, null)
+                    .create().show();
+            return;
+        }
+
+        showAddCoinDialog();
+    }
+
+    private void showAddCoinDialog() {
+        Dialogs.dismissAllowingStateLoss(getFM(), ADD_COIN_DIALOG_TAG);
+        ConfirmAddCoinUnlockWalletDialog.getInstance(selectedCoin, wallet.isEncrypted())
+                .show(getFM(), ADD_COIN_DI
