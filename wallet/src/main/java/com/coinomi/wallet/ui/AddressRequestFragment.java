350476ac233e6774bdabb3c18716714c5589dbfd
windwinding <117351233+windwinding@users.noreply.github.com>
2022-03-15 18:09:08 +0000

    fbcon: don't lose the console font across generic->chip driver switch

--- wallet/src/main/java/com/coinomi/wallet/ui/AddressRequestFragment.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/AddressRequestFragment.java
@@ -24,4 +24,45 @@ import android.widget.Toast;
 import com.coinomi.core.coins.CoinType;
 import com.coinomi.core.coins.FiatType;
 import com.coinomi.core.coins.Value;
-import com.coinomi.core.coins.f
+import com.coinomi.core.coins.families.BitFamily;
+import com.coinomi.core.coins.families.NxtFamily;
+import com.coinomi.core.exceptions.UnsupportedCoinTypeException;
+import com.coinomi.core.uri.CoinURI;
+import com.coinomi.core.util.ExchangeRate;
+import com.coinomi.core.util.GenericUtils;
+import com.coinomi.core.wallet.AbstractAddress;
+import com.coinomi.core.wallet.WalletAccount;
+import com.coinomi.wallet.AddressBookProvider;
+import com.coinomi.wallet.Configuration;
+import com.coinomi.wallet.Constants;
+import com.coinomi.wallet.ExchangeRatesProvider;
+import com.coinomi.wallet.R;
+import com.coinomi.wallet.WalletApplication;
+import com.coinomi.wallet.ui.dialogs.CreateNewAddressDialog;
+import com.coinomi.wallet.ui.widget.AmountEditView;
+import com.coinomi.wallet.util.QrUtils;
+import com.coinomi.wallet.util.ThrottlingWalletChangeListener;
+import com.coinomi.wallet.util.UiUtils;
+import com.coinomi.wallet.util.WeakHandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.coinomi.core.Preconditions.checkNotNull;
+import static com.coinomi.wallet.ExchangeRatesProvider.getRate;
+
+/**
+ *
+ */
+public class AddressRequestFragment extends WalletFragment {
+    private static final Logger log = LoggerFactory.getLogger(AddressRequestFragment.class);
+
+    private static final int UPDATE_VIEW = 0;
+    private stati
