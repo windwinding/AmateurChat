408bf28d35beb704af36bb4448d86fcfa9370df2
windwinding <117351233+windwinding@users.noreply.github.com>
2022-03-18 22:24:21 +0000

    update gh

--- wallet/src/main/java/com/coinomi/wallet/ui/BaseWalletActivity.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/BaseWalletActivity.java
@@ -1,4 +1,56 @@
 package com.coinomi.wallet.ui;
 
 import android.support.v4.app.Fragment;
-impo
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import com.coinomi.core.coins.CoinType;
+import com.coinomi.core.wallet.Wallet;
+import com.coinomi.core.wallet.WalletAccount;
+import com.coinomi.wallet.Configuration;
+import com.coinomi.wallet.WalletApplication;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+/**
+ * @author John L. Jegutanis
+ */
+abstract public class BaseWalletActivity extends AppCompatActivity {
+
+    public WalletApplication getWalletApplication() {
+        return (WalletApplication) getApplication();
+    }
+
+    @Nullable
+    public WalletAccount getAccount(String accountId) {
+        return getWalletApplication().getAccount(accountId);
+    }
+
+    public List<WalletAccount> getAllAccounts() {
+        return getWalletApplication().getAllAccounts();
+    }
+
+    public List<WalletAccount> getAccounts(CoinType type) {
+        return getWalletApplication().getAccounts(type);
+    }
+
+    public List<WalletAccount> getAccounts(List<CoinType> types) {
+        return getWalletApplication().getAccounts(types);
+    }
+
+    public boolean isAccountExists(String accountId) {
+        return getWalletApplication().isAccountExists(accountId);
+    }
+
+    public Configuration getConfiguration() {
+        return getWalletApplication().getConfiguration();
+    }
+
+    public FragmentManager getFM() {
+        return getSupportFragmentManager();
+    }
+
+    public void 
