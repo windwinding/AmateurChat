c8551b72a71eb285d8611669dcc93b5230ea77d3
windwinding <117351233+windwinding@users.noreply.github.com>
2022-03-20 12:07:12 +0000

    Update MANIFEST.MF

--- wallet/src/main/java/com/coinomi/wallet/ui/CoinExchangeListAdapter.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/CoinExchangeListAdapter.java
@@ -18,4 +18,53 @@ import javax.annotation.Nullable;
 /**
  * @author John L. Jegutanis
  */
-public class CoinExchangeListAd
+public class CoinExchangeListAdapter extends BaseAdapter {
+    private final Context context;
+    private final List<CoinType> coins;
+    private final HashMap<String, ExchangeRate> rates;
+
+    public CoinExchangeListAdapter(final Context context, List<CoinType> coins,
+                                   @Nullable Map<String, ExchangeRate> rates) {
+        this.context = context;
+        this.coins = coins;
+        this.rates = new HashMap<>(coins.size());
+        setExchangeRates(rates);
+    }
+
+    public CoinExchangeListAdapter(final Context context, List<CoinType> coins) {
+        this(context, coins, null);
+    }
+
+    @Override
+    public int getCount() {
+        return coins.size();
+    }
+
+    @Override
+    public CoinType getItem(int position) {
+        return coins.get(position);
+    }
+
+    public void setExchangeRates(@Nullable Map<String, ExchangeRate> newRates) {
+        if (newRates != null) {
+            for (ExchangeRate rate : newRates.values()) {
+                if (isRateRelative(rate)) {
+                    this.rates.put(rate.currencyCodeId, rate);
+                }
+            }
+        } else {
+            rates.clear();
+        }
+        notifyDataSetChanged();
+    }
+
+    private boolean isRateRelative(ExchangeRate rate) {
+        if (rate.rate.value1.type instanceof CoinType && coins.contains(rate.rate.value1.type)) {
+            return true;
+        } else if (rate.rate.value2.type instanceof CoinType && coins.contains(rate.rate.value2.type)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Nu
