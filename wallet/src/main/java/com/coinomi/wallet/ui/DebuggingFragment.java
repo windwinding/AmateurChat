47aaaaeab633418016c88d367f05bc99874429b7
windwinding <117351233+windwinding@users.noreply.github.com>
2022-03-21 06:14:39 +0000

    Inline variable

--- wallet/src/main/java/com/coinomi/wallet/ui/DebuggingFragment.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/DebuggingFragment.java
@@ -36,4 +36,51 @@ import static com.coinomi.core.Preconditions.checkNotNull;
  * @author John L. Jegutanis
  */
 public class DebuggingFragment extends Fragment {
-    private static final Str
+    private static final String PROCESSING_DIALOG_TAG = "processing_dialog_tag";
+    private static final String PASSWORD_DIALOG_TAG = "password_dialog_tag";
+
+    private CharSequence password;
+    private PasswordTestTask passwordTestTask;
+    private Wallet wallet;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true); // for the async task
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_debugging, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        ButterKnife.unbind(this);
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onAttach(final Context context) {
+        super.onAttach(context);
+        WalletApplication application = (WalletApplication) context.getApplicationContext();
+        wallet = application.getWallet();
+    }
+
+    @OnClick(R.id.button_execute_password_test)
+    void onExecutePasswordTest() {
+        if (wallet.isEncrypted()) {
+            showUnlockDialog();
+        } else {
+            DialogBuilder.warn(getActivity(), R.string.wallet_is_not_locked_message)
+                    .setPositiveButton(R.string.button_ok, null)
+                    .create().show();
+        }
+    }
+
+    private void showUnlo
