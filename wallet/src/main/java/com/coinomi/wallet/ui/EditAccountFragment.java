9998952188f30b5c321da97f8c639898924e177a
windwinding <117351233+windwinding@users.noreply.github.com>
2022-03-22 22:17:05 +0000

    Update Nuget packages

--- wallet/src/main/java/com/coinomi/wallet/ui/EditAccountFragment.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/EditAccountFragment.java
@@ -22,4 +22,42 @@ import static com.coinomi.wallet.Constants.ARG_ACCOUNT_ID;
 /**
  * @author John L. Jegutanis
  */
-public final class EditAccountF
+public final class EditAccountFragment extends DialogFragment {
+    private static final String FRAGMENT_TAG = EditAccountFragment.class.getName();
+    public static void edit(final FragmentManager fm, WalletAccount account) {
+        final DialogFragment newFragment = EditAccountFragment.instance(account);
+        newFragment.show(fm, FRAGMENT_TAG);
+    }
+
+    private static EditAccountFragment instance(WalletAccount account) {
+        final EditAccountFragment fragment = new EditAccountFragment();
+
+        final Bundle args = new Bundle();
+        args.putString(ARG_ACCOUNT_ID, account.getId());
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    private Context context;
+    private WalletApplication app;
+    private Listener listener;
+
+    @Override
+    public void onAttach(final Context context) {
+        super.onAttach(context);
+        this.context = context;
+        this.app = (WalletApplication) context.getApplicationContext();
+        try {
+            listener = (Listener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement " + Listener.class);
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(final Bundle savedInstanceState) {
+        final Bundle args = getArguments();
+        final WalletAccount account = checkNotNull(app.getAccount(args.getString(ARG_ACCOUNT_ID)));
+        final LayoutInflater inflater = LayoutInflater.from(context);
+        final Di
