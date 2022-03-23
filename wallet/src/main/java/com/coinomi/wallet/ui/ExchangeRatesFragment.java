2bff41218a5b3f30a0f0f4e220624af9186f97f2
windwinding <117351233+windwinding@users.noreply.github.com>
2022-03-23 18:25:03 +0000

    eCryptfs: Revert to a writethrough cache model

--- wallet/src/main/java/com/coinomi/wallet/ui/ExchangeRatesFragment.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/ExchangeRatesFragment.java
@@ -4,4 +4,42 @@ package com.coinomi.wallet.ui;
  * Copyright 2011-2014 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.ResourceCursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.coinomi.core.coins.BitcoinMain;
+import com.coinomi.core.coins.CoinID;
+import com.coinomi.core.coins.CoinType;
+import com.coinomi.core.coins.Value;
+import com.coinomi.core.util.GenericUtils;
+import
