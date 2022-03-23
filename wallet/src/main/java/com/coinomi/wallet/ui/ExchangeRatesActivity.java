19420ab220509276f6cf9b66bd973ca3d706fc2a
windwinding <117351233+windwinding@users.noreply.github.com>
2022-03-23 12:11:54 +0000

    New file structure

--- wallet/src/main/java/com/coinomi/wallet/ui/ExchangeRatesActivity.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/ExchangeRatesActivity.java
@@ -1,27 +1,31 @@
-
 package com.coinomi.wallet.ui;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 
 import com.coinomi.wallet.R;
 
 /**
  * @author John L. Jegutanis
  */
-public class ExchangeHistoryActivity extends BaseWalletActivity {
+public class ExchangeRatesActivity extends BaseWalletActivity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_wrapper);
 
         if (savedInstanceState == null) {
-            ExchangeHistoryFragment fragment = new ExchangeHistoryFragment();
+            ExchangeRatesFragment fragment = new ExchangeRatesFragment();
+            fragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, fragment)
                     .commit();
         }
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(false);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(false);
+        }
     }
 }
---
+++ wallet/src/main/java/com/coinomi/wallet/ui/ExchangeRatesFragment.java
@@ -0,0 +1,7 @@
+package com.coinomi.wallet.ui;
+
+/*
+ * Copyright 2011-2014 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the 
