bf86fc29d9979307d1364999b45fc3140fa9ddd8
windwinding <117351233+windwinding@users.noreply.github.com>
2022-03-28 20:08:25 +0000

    Delete package.json~

--- wallet/src/main/java/com/coinomi/wallet/ui/FeesSettingsFragment.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/FeesSettingsFragment.java
@@ -14,4 +14,53 @@ import com.coinomi.core.coins.Value;
 import com.coinomi.wallet.Configuration;
 import com.coinomi.wallet.R;
 import com.coinomi.wallet.WalletApplication;
-impo
+import com.coinomi.wallet.ui.adaptors.FeesListAdapter;
+import com.coinomi.wallet.ui.dialogs.EditFeeDialog;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnItemClick;
+
+/**
+ * Fragment that restores a wallet
+ */
+public class FeesSettingsFragment extends Fragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private static final String EDIT_FEE_DIALOG = "edit_fee_dialog";
+
+    @Bind(R.id.coins_list) ListView coinList;
+
+    private Configuration config;
+    private Context context;
+    private FeesListAdapter adapter;
+
+    public FeesSettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adapter = new FeesListAdapter(context, config);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_fees_settings_list, container, false);
+        ButterKnife.bind(this, view);
+        coinList.setAdapter(adapter);
+
+        return view;
+    }
+
+    @OnItemClick(R.id.coins_list)
+    void editFee(int currentSelection) {
+        Value fee = (Value) coinList.getItemAtPosition(currentSelection);
+        // Create the fra
