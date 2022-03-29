7fbf4f3072a8c6c105aa044403cca536e6685349
windwinding <117351233+windwinding@users.noreply.github.com>
2022-03-29 12:34:02 +0000

    ignore tmp

--- wallet/src/main/java/com/coinomi/wallet/ui/IntroActivity.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/IntroActivity.java
@@ -10,3 +10,38 @@ import com.coinomi.wallet.R;
 
 public class IntroActivity extends AbstractWalletFragmentActivity
         implements WelcomeFragment.Listener, PasswordConfirmationFragment.Listener,
+        SetPasswordFragment.Listener, SelectCoinsFragment.Listener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_wrapper);
+
+        // If we detected that this device is incompatible
+        if (!getWalletApplication().getConfiguration().isDeviceCompatible()) {
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.incompatible_device_warning_title)
+                    .setMessage(R.string.incompatible_device_warning_message)
+                    .setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setCancelable(false)
+                    .create().show();
+        } else {
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.container, new WelcomeFragment())
+                        .commit();
+            }
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.conta
