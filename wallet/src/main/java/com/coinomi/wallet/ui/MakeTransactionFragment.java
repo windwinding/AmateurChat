234614a97088a17aa1913c6f357a335150b2c40c
windwinding <117351233+windwinding@users.noreply.github.com>
2022-03-31 21:02:07 +0000

    More small fixes.

--- wallet/src/main/java/com/coinomi/wallet/ui/MakeTransactionFragment.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/MakeTransactionFragment.java
@@ -46,4 +46,46 @@ import com.coinomi.wallet.R;
 import com.coinomi.wallet.WalletApplication;
 import com.coinomi.wallet.ui.widget.SendOutput;
 import com.coinomi.wallet.ui.widget.TransactionAmountVisualizer;
-import com.c
+import com.coinomi.wallet.util.Keyboard;
+import com.coinomi.wallet.util.WeakHandler;
+
+import org.bitcoinj.crypto.KeyCrypter;
+import org.bitcoinj.crypto.KeyCrypterException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+
+import javax.annotation.Nullable;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.coinomi.core.Preconditions.checkNotNull;
+import static com.coinomi.core.Preconditions.checkState;
+import static com.coinomi.wallet.Constants.ARG_ACCOUNT_ID;
+import static com.coinomi.wallet.Constants.ARG_EMPTY_WALLET;
+import static com.coinomi.wallet.Constants.ARG_SEND_REQUEST;
+import static com.coinomi.wallet.Constants.ARG_SEND_TO_ACCOUNT_ID;
+import static com.coinomi.wallet.Constants.ARG_SEND_TO_ADDRESS;
+import static com.coinomi.wallet.Constants.ARG_SEND_VALUE;
+import static com.coinomi.wallet.Constants.ARG_TX_MESSAGE;
+import static com.coinomi.wallet.ExchangeRatesProvider.getRates;
+
+/**
+ * This fragment displays a busy message and makes the transaction in the background
+ *
+ */
+public class MakeTransactionFragment extends Fragment {
+    private static final Logger log = LoggerFactory.getLogger(MakeTransactionFragment.class);
+
+    private static final int START_TRADE_TIMEOUT = 0;
+    private static final int UPDATE_TRADE_TIMEOUT = 1;
+    private static final int TRADE_EXPIRED = 2;
+    private static final int STOP_TRADE_TIMEOUT = 3;
+
+    private static final int SAFE_TIMEOUT_MARGIN_SEC = 60;
+
+    // Loader IDs
+    
