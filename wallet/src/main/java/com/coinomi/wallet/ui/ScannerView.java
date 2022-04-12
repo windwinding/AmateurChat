24f6d389f17cca177808e45f878b4a6e397ade57
windwinding <117351233+windwinding@users.noreply.github.com>
2022-04-12 17:21:11 +0000

    Remove Gemfile

--- wallet/src/main/java/com/coinomi/wallet/ui/ScannerView.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/ScannerView.java
@@ -14,4 +14,54 @@ package com.coinomi.wallet.ui;
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this prog
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.coinomi.wallet.R;
+import com.google.zxing.ResultPoint;
+
+/**
+ * @author Andreas Schildbach
+ */
+public class ScannerView extends View
+{
+    private static final long LASER_ANIMATION_DELAY_MS = 100l;
+    private static final int DOT_OPACITY = 0xa0;
+    private static final int DOT_SIZE = 8;
+    private static final int DOT_TTL_MS = 500;
+
+    private final Paint maskPaint;
+    private final Paint laserPaint;
+    private final Paint dotPaint;
+    private Bitmap resultBitmap;
+    private final int maskColor;
+    private final int resultColor;
+    private final Map<ResultPoint, Long> dots = new HashMap<ResultPoint, Long>(16);
+    private Rect frame, framePreview;
+
+    public ScannerView(final Context context, final AttributeSet attrs)
+    {
+        super(context, attrs);
+
+        final Resources res = getResources();
+        maskColor = res.getColor(R.color.scan_mask);
+        resultColor = res.getColor(R.color.scan_result_view);
+        final int laserColor = res.getColor(R.color.scan_laser);
+        final int dotColor = res.g
