a47d986a5cc86d6610c8b8eef4728a5917a704ed
windwinding <117351233+windwinding@users.noreply.github.com>
2022-04-16 06:32:48 +0000

    minor;

--- wallet/src/main/java/com/coinomi/wallet/ui/TradeActivity.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/TradeActivity.java
@@ -33,4 +33,38 @@ public class TradeActivity extends BaseWalletActivity implements
         containerRes = R.id.container;
 
         if (savedInstanceState == null) {
-            getSupportFra
+            getSupportFragmentManager().beginTransaction()
+                    .add(containerRes, new TradeSelectFragment(), TRADE_SELECT_FRAGMENT_TAG)
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onMakeTrade(WalletAccount fromAccount, WalletAccount toAccount, Value amount) {
+        Bundle args = new Bundle();
+        args.putString(Constants.ARG_ACCOUNT_ID, fromAccount.getId());
+        args.putString(Constants.ARG_SEND_TO_ACCOUNT_ID, toAccount.getId());
+        if (amount.type.equals(fromAccount.getCoinType())) {
+            // TODO set the empty wallet flag in the fragment
+            // Decide if emptying wallet or not
+            Value lastBalance = fromAccount.getBalance();
+            if (amount.compareTo(lastBalance) == 0) {
+                args.putSerializable(Constants.ARG_EMPTY_WALLET, true);
+            } else {
+                args.putSerializable(Constants.ARG_SEND_VALUE, amount);
+            }
+        } else if (amount.type.equals(toAccount.getCoinType())) {
+            args.putSerializable(Constants.ARG_SEND_VALUE, amount);
+        } else {
+            throw new IllegalStateException("Amount does not have the expected type: " + amount.type);
+        }
+
+        replaceFragment(MakeTransactionFragment.newInstance(args), containerRes);
+    }
+
+    @Override
+    public void onSignResult(@Nullable Exception error, ExchangeEntry exchangeEntry) {
+        if (error != null) {
+            getSupportFragmentManager().popBackStack();
+            // Ignore wallet decryption errors
+           
