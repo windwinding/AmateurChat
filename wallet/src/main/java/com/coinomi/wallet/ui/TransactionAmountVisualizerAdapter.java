542154282b6bf2c7935e94e51c45a15c7825415b
windwinding <117351233+windwinding@users.noreply.github.com>
2022-04-17 07:45:52 +0000

    add missing copyright

---
+++ wallet/src/main/java/com/coinomi/wallet/ui/TransactionAmountVisualizerAdapter.java
@@ -0,0 +1,12 @@
+package com.coinomi.wallet.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.coinomi.core.coins.CoinType;
+import com.coinomi.core.coins.Value;
+import com.coinomi.core.coins.families.NxtFamily;
+import com.coinomi.core.util.GenericUtils;
