e559281b0251155202ddab870203e933d1487eec
windwinding <117351233+windwinding@users.noreply.github.com>
2022-04-18 19:57:41 +0000

    fixing some stuff

--- wallet/src/main/java/com/coinomi/wallet/ui/UnlockWalletDialog.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/UnlockWalletDialog.java
@@ -18,3 +18,49 @@ import com.coinomi.wallet.R;
 public class UnlockWalletDialog extends DialogFragment {
     private TextView passwordView;
     private Listener listener;
+
+    public static DialogFragment getInstance() {
+        return new UnlockWalletDialog();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            listener = (Listener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.getClass() + " must implement " + Listener.class);
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        listener = null;
+        super.onDetach();
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final LayoutInflater inflater = LayoutInflater.from(getActivity());
+        final View view = inflater.inflate(R.layout.get_password_dialog, null);
+        passwordView = (TextView) view.findViewById(R.id.password);
+
+        return new DialogBuilder(getActivity())
+                .setTitle(R.string.unlock_wallet_title)
+                .setView(view)
+                .setNegativeButton(R.string.button_cancel, dismissListener)
+                .setPositiveButton(R.string.button_ok, okListener)
+                .setCancelable(false)
+                .create();
+    }
+
+    DialogInterface.OnClickListener okListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            if (listener != null) listener.onPassword(passwordView.getText());
+            dismissAllowingStateLoss();
+        }
+    };
+
+    DialogInterface.OnClickListener dism
