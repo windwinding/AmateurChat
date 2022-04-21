23de132c6b4559c460cc3eb5fd82e40885a85f20
windwinding <117351233+windwinding@users.noreply.github.com>
2022-04-21 19:27:50 +0000

    fix texts

--- wallet/src/main/java/com/coinomi/wallet/ui/widget/CoinListItem.java
+++ wallet/src/main/java/com/coinomi/wallet/ui/widget/CoinListItem.java
@@ -32,4 +32,48 @@ public class CoinListItem extends LinearLayout implements Checkable {
     private CoinType type;
 
     public CoinListItem(Context context) {
-  
+        super(context);
+
+        view = LayoutInflater.from(context).inflate(R.layout.coin_list_row, this, true);
+        ButterKnife.bind(this, view);
+    }
+
+    public void setAccount(WalletAccount account) {
+        this.type = account.getCoinType();
+        title.setText(account.getDescriptionOrCoinName());
+        icon.setImageResource(WalletUtils.getIconRes(account));
+    }
+
+    public void setCoin(CoinType type) {
+        this.type = type;
+        title.setText(type.getName());
+        icon.setImageResource(WalletUtils.getIconRes(type));
+    }
+
+    public void setExchangeRate(ExchangeRate exchangeRate) {
+        if (exchangeRate != null && type != null) {
+            Value localAmount = exchangeRate.rate.convert(type.oneCoin());
+            setFiatAmount(localAmount);
+        } else {
+            amount.setVisibility(View.GONE);
+        }
+    }
+
+    public void setAmount(Value value) {
+        amount.setAmount(GenericUtils.formatCoinValue(value.type, value, true));
+        amount.setSymbol(value.type.getSymbol());
+        amount.setVisibility(View.VISIBLE);
+    }
+
+    private void setFiatAmount(Value value) {
+        amount.setAmount(GenericUtils.formatFiatValue(value));
+        amount.setSymbol(value.type.getSymbol());
+        amount.setVisibility(View.VISIBLE);
+    }
+
+    public void setAmountSingleLine(boolean isSingleLine) {
+        amount.setSingleLine(isSingleLine);
+    }
+
+    @Override
+    public void setChecked(bo
