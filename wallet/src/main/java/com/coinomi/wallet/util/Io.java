64145925d06ccd2d9fdde38ddaee2092f1e8018b
windwinding <117351233+windwinding@users.noreply.github.com>
2022-04-29 06:03:41 +0000

    improve search

--- wallet/src/main/java/com/coinomi/wallet/util/Io.java
+++ wallet/src/main/java/com/coinomi/wallet/util/Io.java
@@ -30,4 +30,41 @@ public class Io
         long count = 0;
         int n = 0;
         while (-1 != (n = reader.read(buffer)))
-       
+        {
+            builder.append(buffer, 0, n);
+            count += n;
+
+            if (maxChars != 0 && count > maxChars)
+                throw new IOException("Read more than the limit of " + maxChars + " characters");
+        }
+        return count;
+    }
+
+    public static final long copy(@Nonnull final InputStream is, @Nonnull final OutputStream os) throws IOException
+    {
+        final byte[] buffer = new byte[1024];
+        long count = 0;
+        int n = 0;
+        while (-1 != (n = is.read(buffer)))
+        {
+            os.write(buffer, 0, n);
+            count += n;
+        }
+        return count;
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public static void chmod(@Nonnull final File path, final int mode)
+    {
+        try
+        {
+            final Class fileUtils = Class.forName("android.os.FileUtils");
+            final Method setPermissions = fileUtils.getMethod("setPermissions", String.class, int.class, int.class, int.class);
+            setPermissions.invoke(null, path.getAbsolutePath(), mode, -1, -1);
+        }
+        catch (final Exception x)
+        {
+            log.info("problem using undocumented chmod api", x);
+        }
+    }
+}
---
+++ wallet/src/main/java/com/coinomi/wallet/util/Keyboard.java
@@ -0,0 +1,14 @@
+package com.coinomi.wallet.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+
+/**
+ * @author John L. Jegutanis
+ */
+public class Keyboard {
+
+    // FIXME causes problems in older Androids
