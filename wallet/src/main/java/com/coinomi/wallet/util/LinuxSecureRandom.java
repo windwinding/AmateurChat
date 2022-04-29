66346fc2b5a6ee6acb7bbf7233c9ab1362b5aa1d
windwinding <117351233+windwinding@users.noreply.github.com>
2022-04-29 09:47:57 +0000

    add new dependencies

--- wallet/src/main/java/com/coinomi/wallet/util/LinuxSecureRandom.java
+++ wallet/src/main/java/com/coinomi/wallet/util/LinuxSecureRandom.java
@@ -10,4 +10,41 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.coinomi.wallet.util;
+
+import org.slf4j.*;
+
+import java.io.*;
+import java.security.*;
+
+/**
+ * A SecureRandom implementation that is able to override the standard JVM provided implementation, and which simply
+ * serves random numbers by reading /dev/urandom. That is, it delegates to the kernel on UNIX systems and is unusable on
+ * other platforms. Attempts to manually set the seed are ignored. There is no difference between seed bytes and
+ * non-seed bytes, they are all from the same source.
+ */
+public class LinuxSecureRandom extends SecureRandomSpi {
+    private static final FileInputStream urandom;
+
+    private static class LinuxSecureRandomProvider extends Provider {
+        public LinuxSecureRandomProvider() {
+            super("LinuxSecureRandom", 1.0, "A Linux specific random number provider that uses /dev/urandom");
+            put("SecureRandom.LinuxSecureRandom", LinuxSecureRandom.class.getName());
+        }
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(LinuxSecureRandom.class);
+
+    static {
+        try {
+            File file = new File("/dev/urandom");
+            // This stream is deliberately leaked.
+            urandom = new FileInputStream(file);
+            if (urandom.read() == -1)
+                throw new RuntimeException("/dev/urandom not readable?");
+            // Now override the default SecureRandom implementation with this one.
+          
